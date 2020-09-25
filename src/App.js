fa64c7e22569fc201a27bcb458b9b0c0887f943a
kamoto0427 <takahiro.08039@gmail.com>
2020-09-25 21:53:40 +0900

    TodoListのコンポーネント化

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, Button, Table } from 'reactstrap';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -10,7 +12,6 @@ function App() {
   const addTodo = text => {
     const newTodos = [...todos, {text, complete: false}]
     setTodos(newTodos)
-    console.log(newTodos)
   }
 
   const removeTodo = index => {
@@ -31,28 +32,10 @@ function App() {
       <Container>
         <h1 className="mt-4">To doリスト</h1>
         <TodoForm addTodo={addTodo}/>
-      </Container>
-      <Container>
-        <Table>
-          <tbody>
-            {todos && todos.map((todo, index) => (
-            <tr key={index}>
-              <th className="text-left" style={{ textDecoration: todo.complete ? "line-through" : "" }}>
-                {todo.text}
-              </th>
-              <td className="text-right">
-                <Button 
-                  color={todo.complete ? "secondary" : "success"}
-                  className="mr-2" onClick={() => completeTodo(index)}>
-                    {todo.complete ? '完了' : '未完了'}
-                </Button>
-                <Button color="danger" onClick={() => removeTodo(index)}>削除</Button>
-              </td>
-            </tr>
-            ))}
-
-          </tbody>
-        </Table>
+        <TodoList 
+          todos={todos}
+          removeTodo={removeTodo}
+          completeTodo={completeTodo}/>
       </Container>
     </div>
   );
