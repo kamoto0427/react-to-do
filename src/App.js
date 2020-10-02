139ca783fe589f89c309de55747a2d3b658f4409
kamoto0427 <takahiro.08039@gmail.com>
2020-10-02 23:48:36 +0900

    TodosContextへ移行

--- src/App.js
+++ src/App.js
@@ -6,26 +6,6 @@ import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([])
-
-
-  const addTodo = text => {
-    const newTodos = [...todos, {text, complete: false}]
-    setTodos(newTodos)
-  }
-
-  const removeTodo = index => {
-    const newTodos = [...todos]
-    newTodos.splice(index, 1)
-    setTodos(newTodos)
-  }
-
-  const completeTodo = index => {
-    const newTodos = [...todos]
-    newTodos[index].complete = !newTodos[index].complete
-    setTodos(newTodos)
-    console.log(newTodos)
-  }
 
   return (
     <div className="App">
--- src/contexsts/TodosContexst.js
+++ src/contexsts/TodosContexst.js
@@ -3,6 +3,24 @@ import React, { createContext,  useState } from 'react'
 export const TodosContext = createContexst()
 
 const TodosContextProvider = (props) => {
+  const [todos, setTodos] = useState([])
+  const addTodo = text => {
+    const newTodos = [...todos, {text, complete: false}]
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].complete = !newTodos[index].complete
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
   return (
     <TodosContext.Provider>
       {props.children}
